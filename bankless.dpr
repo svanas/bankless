ca47306470cc5866e407fd8759ae06b0a5e085e5
svanas <svanas@runbox.com>
2020-06-06 11:11:31 +0200

    Project skeleton. Unknown version history.

---
+++ bankless.dpr
@@ -0,0 +1,14 @@
+program bankless;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  main in 'main.pas' {frmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
