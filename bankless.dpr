bc88a0548603bca26d706d5e4d26be93af12eee6
Stefan <dutchdelphidude@gmail.com>
2024-08-21 13:03:35 +0200

    v1.0.20

--- bankless.dpr
+++ bankless.dpr
@@ -3,14 +3,12 @@ program bankless;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  FMX.Types,
   main in 'main.pas' {frmMain},
   apyCache in 'apyCache.pas';
 
 {$R *.res}
 
 begin
-  GlobalUseMetal := True;
   Application.Initialize;
   Application.CreateForm(TfrmMain, frmMain);
   Application.Run;
